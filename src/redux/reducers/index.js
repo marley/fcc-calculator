97a42861ce4df7ba0928dd9253dc0990d5cd118d
Marley Alford <marleycodes@gmail.com>
2021-04-23 01:21:13 -0400

    Only allow one decimal per number

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -15,6 +15,7 @@ const initialState = {
 
 const evaluate = (num, operator, total) => {
   // params: int/float, string, int/float
+  console.log({ num, operator, total });
   let mathNum = new MathNum(`${num}`);
   let mathTotal = new MathNum(`${total}`);
   switch (operator) {
@@ -31,31 +32,39 @@ const evaluate = (num, operator, total) => {
   }
 };
 
-// const updateCurrentNum = (state, payload) => {
-//   /* As you press number keys, update */
-//   let currentNum = `${state.currentNum}${payload}`;
-//   let input = `${state.input}${payload}`;
-//   return { currentNum };
-// };
-
-const updateTotal = (state, payload) => {
+const updateTotal = (state, payload, nextNumNegative) => {
   /* if we reach operator or end of expression, then
   evaluate exp so far */
-  let total = evaluate(state.currentNum, state.lastOperator, state.total);
+  console.log("Update total:", state.lastOperator);
+  let currentNum = state.currentNum;
+  let total = state.total;
+  if (currentNum.length > 0 && currentNum !== "-") {
+    total = evaluate(currentNum, state.lastOperator, state.total);
+    currentNum = "";
+  } else if (nextNumNegative) {
+    currentNum = "-";
+  }
   let lastOperator = payload; // update to next operator
-  let currentNum = "";
+
   return { total, lastOperator, currentNum };
 };
 
 const calculatorReducer = (state = initialState, action) => {
-  console.log("REDUCER:");
+  console.log("============REDUCER:============");
+  console.log(state);
   switch (action.type) {
     case PRESS_NUM: {
-      console.log("NUM", action.payload);
       let payload = action.payload;
       let input = state.input;
       let currentNum = state.currentNum;
-      if (input !== "0") {
+      console.log(payload);
+      if (payload === ".") {
+        if (currentNum.indexOf(".") < 0) {
+          // if no other decimal points, then can append
+          input = `${state.input}${payload}`;
+          currentNum = `${state.currentNum}${payload}`;
+        }
+      } else if (input !== "0") {
         input = `${state.input}${payload}`;
         currentNum = `${state.currentNum}${payload}`;
       } else {
@@ -75,20 +84,27 @@ const calculatorReducer = (state = initialState, action) => {
       let payload = action.payload;
       let input = state.input;
       let numbersOnly = /[0-9.]/;
+      let nextNumNegative = false;
       if (numbersOnly.test(input[input.length - 1])) {
         // append operator after any number
         input = `${input}${payload}`;
       } else {
-        if (payload === "-" && input[input.length - 1] === "-") {
-          // change two minu to plus
-          input = `${input.slice(0, input.length - 1)}+`;
-          payload = "+";
+        // 1 + 2 +-6/
+        if (payload === "-") {
+          input = `${input}${payload}`;
+          nextNumNegative = true;
+          console.log(nextNumNegative);
+          payload = state.lastOperator;
         } else {
           // default to last operator entered with new operator
           input = `${input.slice(0, input.length - 1)}${payload}`;
         }
       }
-      let { total, lastOperator, currentNum } = updateTotal(state, payload);
+      let { total, lastOperator, currentNum } = updateTotal(
+        state,
+        payload,
+        nextNumNegative
+      );
       return { ...state, input, total, currentNum, lastOperator };
     }
     case PRESS_EQUALS: {
@@ -97,11 +113,10 @@ const calculatorReducer = (state = initialState, action) => {
       // updateTotal
       let { total, lastOperator, currentNum } = updateTotal(state, "+");
       // set input to total
-      return { ...state, input: total, total, currentNum, lastOperator };
+      return { ...state, input: `${total}`, total, currentNum, lastOperator };
     }
     case PRESS_CLEAR: {
       return {
-        ...state,
         input: "0",
         total: 0,
         currentNum: "0",
